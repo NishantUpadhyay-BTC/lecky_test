6696065a313b94bc4248dd59667766ef90948b7b
Olivier van Hees <oliviervanhees@gmail.com>
2015-04-05 12:06:19 +0200

    removed bug

--- app/mailers/model_mailer.rb
+++ app/mailers/model_mailer.rb
@@ -8,14 +8,13 @@ class ModelMailer < ActionMailer::Base
   #
   def new_pin_notification(pin)
     @pin = pin
-    mail to: pin.user.email, subject: "#{@pin.user.name}, jouw restaurant #{@pin.description} is toegevoegd.", bcc: "oliviervanhees@gmail.com"
+    mail to: pin.user.email, subject: "#{@pin.user.name}, your restaurant #{@pin.description} is added.", bcc: "oliviervanhees@gmail.com"
   end
 
   def new_like_notification(pin)
     @pin = pin
     @user = pin.user
-  
-    mail to: pin.user.email, subject: "#{@pin.user.name}, jouw restaurant #{@pin.description} wordt lekker gevonden door anderen.", bcc: "oliviervanhees@gmail.com"
+    mail to: pin.user.email, subject: "#{@pin.user.name}, your restaurant #{@pin.description} is now liked by others.", bcc: "oliviervanhees@gmail.com"
   end
 
 end
