a68f33b3793b1f8970c53a9444fc9b5c0fa5c0a7
Olivier van Hees <oliviervanhees@gmail.com>
2015-04-09 00:06:21 +0200

    changed migration files

--- db/migrate/20150408152752_add_confirmable_to_devise.rb
+++
@@ -1,7 +0,0 @@
-class AddConfirmableToDevise < ActiveRecord::Migration
-  def change
-    change_table(:users) do |t| 
-       t.remove   :confirmation_token, :confirmed_at, :confirmation_sent_at	
-    end
-end
-end
--- db/migrate/20150408164914_add_confirmable_to_devise3.rb
+++
@@ -1,7 +0,0 @@
-class AddConfirmableToDevise3 < ActiveRecord::Migration
-  def change
-  	 change_table(:users) do |t| 
-  	 	t.string   :unconfirmed_email
-  end
-end
-end
