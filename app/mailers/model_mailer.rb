e1fec7c1736b5b5bd26079adaa1427706c840ff6
ParthivPatel-BTC <parthiv.patel@botreeconsulting.com>
2015-05-12 17:43:28 +0530

    #17 - Send an email when an user that you follow likes a restaurant

--- app/mailers/model_mailer.rb
+++ app/mailers/model_mailer.rb
@@ -11,10 +11,11 @@ class ModelMailer < ActionMailer::Base
     mail to: pin.user.email, subject: "#{@pin.user.name}, your restaurant #{@pin.description} is added.", bcc: "oliviervanhees@gmail.com"
   end
 
-  def new_like_notification(pin)
+  def new_like_notification(pin, follower, current_user)
     @pin = pin
-    @user = pin.user
-    mail to: pin.user.email, subject: "#{@pin.user.name}, your restaurant #{@pin.description} is now liked by others.", bcc: "oliviervanhees@gmail.com"
+    @follower = follower
+    @current_user = current_user
+    mail to: @follower.email, subject: "#{@follower.name}, your restaurant #{@pin.description} is now liked by others.", bcc: "oliviervanhees@gmail.com"
   end
 
   def new_follower_notification(user, current_user)
