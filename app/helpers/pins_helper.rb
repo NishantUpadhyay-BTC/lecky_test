6e73c0d68ceed605576a8ef4503baf54fe37308b
ParthivPatel-BTC <parthiv.patel@botreeconsulting.com>
2015-05-14 15:05:24 +0530

    #14 - Search on location

--- app/helpers/pins_helper.rb
+++ app/helpers/pins_helper.rb
@@ -1,2 +1,5 @@
 module PinsHelper
+  def find_nearby_distance(latitude_longitude, restaurant)
+    Geocoder::Calculations.distance_between(latitude_longitude, [restaurant.latitude, restaurant.longitude]).round(2)
+  end
 end
